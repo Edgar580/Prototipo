58f233f2b289c827bf09dee87eb57c35b875609a
Edgar Vasquez <EdgarVasquez@gritvap.com>
2020-12-15 17:49:10 -0400

    Modificacion a Login

--- Prototipo/Inicio.Designer.cs
+++ Prototipo/Inicio.Designer.cs
@@ -32,19 +32,20 @@ namespace Prototipo
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Inicio));
             this.richTextBox_ID = new System.Windows.Forms.RichTextBox();
             this.panel_login = new System.Windows.Forms.Panel();
-            this.richTextBox_Clave = new System.Windows.Forms.RichTextBox();
-            this.label_ID = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.LineaVentas1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label_ID = new System.Windows.Forms.Label();
+            this.richTextBox_Clave = new System.Windows.Forms.RichTextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label2 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.label4 = new System.Windows.Forms.Label();
-            this.LineaVentas1 = new System.Windows.Forms.Panel();
             this.btnCerrar = new System.Windows.Forms.PictureBox();
-            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
             this.panel_login.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panel1.SuspendLayout();
@@ -79,27 +80,55 @@ namespace Prototipo
             this.panel_login.Size = new System.Drawing.Size(232, 242);
             this.panel_login.TabIndex = 1;
             // 
-            // richTextBox_Clave
+            // panel3
             // 
-            this.richTextBox_Clave.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.richTextBox_Clave.Location = new System.Drawing.Point(37, 116);
-            this.richTextBox_Clave.MaxLength = 7;
-            this.richTextBox_Clave.Multiline = false;
-            this.richTextBox_Clave.Name = "richTextBox_Clave";
-            this.richTextBox_Clave.Size = new System.Drawing.Size(147, 23);
-            this.richTextBox_Clave.TabIndex = 2;
-            this.richTextBox_Clave.Text = "";
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panel3.Location = new System.Drawing.Point(104, 208);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(10, 31);
+            this.panel3.TabIndex = 215;
             // 
-            // label_ID
+            // LineaVentas1
             // 
-            this.label_ID.AutoSize = true;
-            this.label_ID.Font = new System.Drawing.Font("Times New Roman", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label_ID.ForeColor = System.Drawing.SystemColors.ControlLight;
-            this.label_ID.Location = new System.Drawing.Point(85, 25);
-            this.label_ID.Name = "label_ID";
-            this.label_ID.Size = new System.Drawing.Size(58, 18);
-            this.label_ID.TabIndex = 2;
-            this.label_ID.Text = "Usuario";
+            this.LineaVentas1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.LineaVentas1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.LineaVentas1.Location = new System.Drawing.Point(3, 200);
+            this.LineaVentas1.Name = "LineaVentas1";
+            this.LineaVentas1.Size = new System.Drawing.Size(225, 10);
+            this.LineaVentas1.TabIndex = 214;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label4.Location = new System.Drawing.Point(139, 214);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 18);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Registrar";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label3.Location = new System.Drawing.Point(3, 214);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 18);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Cambiar clave";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.button1.Location = new System.Drawing.Point(60, 162);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 33);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Acceder";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
@@ -112,36 +141,37 @@ namespace Prototipo
             this.label1.TabIndex = 3;
             this.label1.Text = "Clave";
             // 
-            // button1
+            // label_ID
             // 
-            this.button1.Font = new System.Drawing.Font("Times New Roman", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
-            this.button1.Location = new System.Drawing.Point(60, 162);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(92, 33);
-            this.button1.TabIndex = 5;
-            this.button1.Text = "Acceder";
-            this.button1.UseVisualStyleBackColor = true;
+            this.label_ID.AutoSize = true;
+            this.label_ID.Font = new System.Drawing.Font("Times New Roman", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_ID.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label_ID.Location = new System.Drawing.Point(85, 25);
+            this.label_ID.Name = "label_ID";
+            this.label_ID.Size = new System.Drawing.Size(58, 18);
+            this.label_ID.TabIndex = 2;
+            this.label_ID.Text = "Usuario";
+            // 
+            // richTextBox_Clave
+            // 
+            this.richTextBox_Clave.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox_Clave.Location = new System.Drawing.Point(37, 116);
+            this.richTextBox_Clave.MaxLength = 7;
+            this.richTextBox_Clave.Multiline = false;
+            this.richTextBox_Clave.Name = "richTextBox_Clave";
+            this.richTextBox_Clave.Size = new System.Drawing.Size(147, 23);
+            this.richTextBox_Clave.TabIndex = 2;
+            this.richTextBox_Clave.Text = "";
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(290, 84);
+            this.pictureBox1.Location = new System.Drawing.Point(290, 43);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(321, 158);
             this.pictureBox1.TabIndex = 5;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Times New Roman", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.SystemColors.ControlLight;
-            this.label3.Location = new System.Drawing.Point(3, 214);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(100, 18);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "Cambiar clave";
             // 
             // label2
             // 
@@ -164,34 +194,6 @@ namespace Prototipo
             this.panel1.Size = new System.Drawing.Size(643, 27);
             this.panel1.TabIndex = 6;
             // 
-            // panel2
-            // 
-            this.panel2.BackColor = System.Drawing.Color.DarkRed;
-            this.panel2.Location = new System.Drawing.Point(0, 280);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(643, 27);
-            this.panel2.TabIndex = 7;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Times New Roman", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.ForeColor = System.Drawing.SystemColors.ControlLight;
-            this.label4.Location = new System.Drawing.Point(139, 214);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(67, 18);
-            this.label4.TabIndex = 7;
-            this.label4.Text = "Registrar";
-            // 
-            // LineaVentas1
-            // 
-            this.LineaVentas1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.LineaVentas1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.LineaVentas1.Location = new System.Drawing.Point(3, 200);
-            this.LineaVentas1.Name = "LineaVentas1";
-            this.LineaVentas1.Size = new System.Drawing.Size(225, 10);
-            this.LineaVentas1.TabIndex = 214;
-            // 
             // btnCerrar
             // 
             this.btnCerrar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
@@ -205,14 +207,24 @@ namespace Prototipo
             this.btnCerrar.TabStop = false;
             this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
             // 
-            // panel3
+            // panel2
             // 
-            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.panel3.Location = new System.Drawing.Point(104, 208);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(10, 31);
-            this.panel3.TabIndex = 215;
+            this.panel2.BackColor = System.Drawing.Color.DarkRed;
+            this.panel2.Location = new System.Drawing.Point(0, 280);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(643, 27);
+            this.panel2.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label5.Location = new System.Drawing.Point(344, 209);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(228, 33);
+            this.label5.TabIndex = 216;
+            this.label5.Text = "Indice academico";
             // 
             // Inicio
             // 
@@ -220,6 +232,7 @@ namespace Prototipo
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.ClientSize = new System.Drawing.Size(641, 304);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.pictureBox1);
@@ -238,6 +251,7 @@ namespace Prototipo
             this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.btnCerrar)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -258,6 +272,7 @@ namespace Prototipo
         private System.Windows.Forms.Panel LineaVentas1;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.PictureBox btnCerrar;
+        private System.Windows.Forms.Label label5;
     }
 }
 
