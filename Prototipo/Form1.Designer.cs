a551e4ca3dd0927ee3ab77a51778d3731a347563
Alan <ddoladdd@hotmail.com>
2020-12-15 17:09:13 -0400

    Hola

--- Prototipo/Form1.Designer.cs
+++ Prototipo/Form1.Designer.cs
@@ -29,20 +29,48 @@ namespace Prototipo
         /// </summary>
         private void InitializeComponent()
         {
+            this.richTextBox_ID = new System.Windows.Forms.RichTextBox();
+            this.panel_login = new System.Windows.Forms.Panel();
+            this.panel_login.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // richTextBox_ID
+            // 
+            this.richTextBox_ID.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox_ID.Location = new System.Drawing.Point(46, 37);
+            this.richTextBox_ID.MaxLength = 7;
+            this.richTextBox_ID.Multiline = false;
+            this.richTextBox_ID.Name = "richTextBox_ID";
+            this.richTextBox_ID.Size = new System.Drawing.Size(147, 23);
+            this.richTextBox_ID.TabIndex = 1;
+            this.richTextBox_ID.Text = "";
+            // 
+            // panel_login
+            // 
+            this.panel_login.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(211)))), ((int)(((byte)(8)))), ((int)(((byte)(18)))));
+            this.panel_login.Controls.Add(this.richTextBox_ID);
+            this.panel_login.Location = new System.Drawing.Point(122, 44);
+            this.panel_login.Name = "panel_login";
+            this.panel_login.Size = new System.Drawing.Size(234, 340);
+            this.panel_login.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(523, 378);
+            this.ClientSize = new System.Drawing.Size(479, 428);
+            this.Controls.Add(this.panel_login);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.panel_login.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox_ID;
+        private System.Windows.Forms.Panel panel_login;
     }
 }
 
