98da5e28d7c4bb92064b47f73904c5e99c08b61e
JOSE ANTONIO MIRANDA ALMEYDA <1096359@est.intec.edu.do>
2020-12-16 22:51:11 -0400

    acabé siendo el final

--- Prototipo/Estudiante.cs
+++ Prototipo/Estudiante.cs
@@ -21,5 +21,25 @@ namespace Prototipo
         {
 
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Estudiantes_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
