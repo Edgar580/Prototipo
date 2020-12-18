eab67083ad65e7a13c520ab8fc6d573006cf1537
SAHIRA TEJADA PENA <1098917@est.intec.edu.do>
2020-12-17 21:38:22 -0500

    hola

--- Prototipo/Profesor.cs
+++ Prototipo/Profesor.cs
@@ -16,5 +16,15 @@ namespace Prototipo
         {
             InitializeComponent();
         }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
